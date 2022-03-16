771809a3efdf18fbe8254ffa75e73b8c98e9f488
Alexandria <xdriadev@gmail.com>
2022-03-15 23:28:36 -0400

    Commiting assets for Mindless Bouncing

---
+++ Assets/Scripts/BallHandler.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float detachDelay;
+    [SerializeField] private float respawnDelay;
+
+    private Rigidbody2D currentBallRigidbody;
+    private SpringJoint2D currentBallSpringJoint;
+
+    private Camera mainCamera;
+    private bool isDragging;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        SpawnNewBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentBallRigidbody==null){return;}
+        if(!Touchscreen.current.press.isPressed)
+        {
+            if(isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
+            return;
+        }
+
+        isDragging = true;
+        currentBallRigidbody.isKinematic = true;
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidbody.position = worldPosition;
+    }
+
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+        currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+        currentBallSpringJoint.connectedBody = pivot;
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
+        Invoke(nameof(SpawnNewBall), respawnDelay);
+    }
+}
+ 
